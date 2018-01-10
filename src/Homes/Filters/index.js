da29f90dbe27fc28b08c3472e6ff7b3e22b4c0ac
Denis S <skdenx@gmail.com>
2018-01-11 02:01:06 +0400

    Add react-router,  Fix import urls

---
+++ src/Homes/Filters/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const Filter = styled.button`
+  display: inline-block;
+  font-family: "CircularAir", Helvetica Neue, Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  line-height: 16px;
+  border: 1px solid rgba(72, 72, 72, 0.2);
+  box-sizing: border-box;
+  border-radius: 4px;
+`;
+
+export default props => {
+  return (
+
+  )
+}
